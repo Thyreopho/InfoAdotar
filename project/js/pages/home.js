3d53f562c9d6f543006c5cb13f52553cff721ceb
João Marcos <jmarcosomjm@gmail.com>
2020-06-27 16:12:37 -0300

    Atualizacao Local storage na Home

--- project/js/pages/home.js
+++ project/js/pages/home.js
@@ -1,47 +1,39 @@
-//Versão do João
-$.getJSON("../js/devDataBase.json", function (data) {
-let tela = document.getElementById("fArea");
-let content="";
-data=data.TbFaq;
-for(let i=0;i<3;i++)
-{ 
-    content=content+
-    `
-    <div style="margin:30px 30px" class="card">
-        <div class="card-header" id="heading${i}">
-            <h2 class="mb-0">
-                <button class="btn  btn-block text-left" type="button" data-toggle="collapse"
-                    data-target="#C${i}" aria-expanded="true" aria-controls="C1">
-                    ${data[i].titulo}
-                </button>
-            </h2>
-        </div>
-        <div id="C${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#fArea">
-            <div class="card-body fbody">
-            ${data[i].texto}
-                </div>
-            </div>
-        </div>
-    `
-}
-tela.innerHTML=content;
-}).done();
-
-//Versão do Felipe
-
-//#region Controllers
-
+//João Marcos
 var faqController;
-
-//#endregion
-
-//Espaço que é chamado quando o susário é verificado
+//É executado quando o usuario faz loguin
 $(document).on("userLogged", function () {
-    //#region Setting Controllers
-
-        //Variavel faqController permite o acesso aos dados da FAQ
-        //Obs: olhar js/controller/faqController.js
-        faqController = new FaqController(faqTable);
-
-    //#endregion
-});
+    faqController = new FaqController(faqTable);
+    let tela = document.getElementById("fArea");
+    let content="";
+    faqController.readAll(function(data){
+        for(let i=0;i<3;i++) // popula os FAQs com as 3 primeiras perguntas do local storage
+        { 
+            content=content+
+            `
+            <div style="margin:30px 30px" class="card">
+                <div class="card-header" id="heading${i}">
+                    <h2 class="mb-0">
+                        <button class="btn  btn-block text-left" type="button" data-toggle="collapse"
+                            data-target="#C${i}" aria-expanded="true" aria-controls="C1">
+                            ${data[i].titulo}
+                        </button>
+                    </h2>
+                </div>
+                <div id="C${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#fArea">
+                    <div class="card-body fbody">
+                    ${data[i].texto}
+                        </div>
+                    </div>
+                </div>
+            `
+        }
+        tela.innerHTML=content;
+        console.log("Sucesso");    
+    },
+    function() {
+        console.log("Algo de errado ocorreu");
+    }
+    );
+    });
+    
+    
