24d36c3849f503c7b09445e4b076b8f70cfc649e
rafaelpmesquita <you@example.com>
2020-06-28 10:08:39 -0300

    Tela cadastro

--- project/js/pages/cadastro.js
+++ project/js/pages/cadastro.js
@@ -6,26 +6,57 @@
 
 //#endregion
 
-//Espaço que é chamado quando o susário é verificado
+function NovoUsuario() {
+    let nome = $("#cadastroNome").val();
+    let email = $("#cadastroEmail").val();
+    let senha = $("#cadastroPassword").val();
+    let verificasenha = $("#cadastroConfirmPassword").val();
+    if (senha == verificasenha) {
+      let cadastraUsuario = new User(nome,email,senha,1);
+
+        userController.create(cadastraUsuario,
+            function () {
+                alert("Cadastro realizado com sucesso!");
+                window.location.href='index.html';
+            
+            },
+            function (msg) {
+                alert("Erro ao cadastrar no sistema:" + msg);
+            }
+    
+
+    )
+        }else{
+            alert("Senhas diferentes.Tente novamente.")
+        }
+           
+}
+
+
+
+//Espaço que é chamado quando o usário é verificado
 $(document).on("userNotLogged", function () {
     //#region Setting Controllers
 
-        //Variavel userController permite o acesso aos dados dos usuários
-        //Obs: olhar js/controller/userController.js
+    //Variavel userController permite o acesso aos dados dos usuários
+    //Obs: olhar js/controller/userController.js
 
     //#endregion
 
+    $("#btncadastrar").click(NovoUsuario);
+
+
     //Exemplos:
-        
-        //Usando o userController
-        userController.readAll(
-            function (list) {
-                for (let i = 0; i < list.length; i++) {
-                    console.log(`${i + 1}º usuário: ${lits[i].nome}`);
-                }
-            },
-            function () {
-                console.log("Ocorreu um erro ao listar os usuários");
+
+    //Usando o userController
+    userController.readAll(
+        function (list) {
+            for (let i = 0; i < list.length; i++) {
+                console.log(`${i + 1}º usuário: ${list[i].nome}`);
             }
-        );
+        },
+        function () {
+            console.log("Ocorreu um erro ao listar os usuários");
+        }
+    );
 });
